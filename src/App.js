6bdfe9d2aa734b30a52c36286db0abd5c69f8678
Mike <mycelius23@gmail.com>
2020-02-19 19:17:21 -0700

    added apexcharts for horizontal charts.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import StackedChart1 from './StackedChart1.js';
 import Navbar from './Navbar.js';
 import PieChart1 from './PieChart1.js';
 import PieChart2 from './PieChart2.js';
+import BarChart1 from './BarChart1.js';
 
 class App extends React.Component {
   render() {
@@ -21,6 +22,7 @@ class App extends React.Component {
       <StackedChart1 />
       <PieChart1 />
       <PieChart2 />
+      <BarChart1 />
     </div>
   );
 }
